ab319677f6ea0befddf8b35728c34500e75a64ab
Partially Sorted <partially.sorted@gmail.com>
2022-02-11 06:49:22 -0600

    Expanded tokens for Apricot.

--- src/adapters/apricot.ts
+++ src/adapters/apricot.ts
@@ -1,5 +1,6 @@
 import { Connection } from "@solana/web3.js";
 import { createAssetPoolLoader, TokenID } from "@apricot-lend/sdk-ts";
+import TOKENS from '../tokens.json';
 import { AssetRate, ProtocolRates } from '../types';
 
 export async function fetch(): Promise<ProtocolRates> {
@@ -7,17 +8,26 @@ export async function fetch(): Promise<ProtocolRates> {
   const assetPoolLoader = await createAssetPoolLoader(connection);
 
   const rates: AssetRate[] = (await Promise.all([
+    assetPoolLoader.getAssetPool(TokenID.APT),
     assetPoolLoader.getAssetPool(TokenID.BTC),
     assetPoolLoader.getAssetPool(TokenID.ETH),
+    assetPoolLoader.getAssetPool(TokenID.FTT),
+    assetPoolLoader.getAssetPool(TokenID.mSOL),
+    assetPoolLoader.getAssetPool(TokenID.ORCA),
+    assetPoolLoader.getAssetPool(TokenID.RAY),
     assetPoolLoader.getAssetPool(TokenID.SOL),
+    assetPoolLoader.getAssetPool(TokenID.SRM),
     assetPoolLoader.getAssetPool(TokenID.USDC),
+    assetPoolLoader.getAssetPool(TokenID.USDT),
+    assetPoolLoader.getAssetPool(TokenID.USTv2),
   ])).map((assetPool) => {
+    const token = TOKENS.find((token) => { return token.mint === assetPool?.mintKey.toBase58(); })
     return {
-      asset: assetPool?.tokenName,
+      asset: token!.symbol,
       deposit: assetPool?.depositRate,
       borrow: assetPool?.borrowRate,
     } as AssetRate;
-  });
+});
 
   return {
     protocol: 'apricot',
